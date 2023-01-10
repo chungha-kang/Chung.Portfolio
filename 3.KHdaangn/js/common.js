c782467660a956dadfde78d557fddf5f080fa7af
chungha-kang <cubone89@gmail.com>
2023-01-10 11:23:13 +0900

    HTML in HTML TEST3

---
+++ 3.KHdaangn/js/common.js
@@ -0,0 +1,5 @@
+<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+    $(document).ready( function() {
+        $("#header").load("module/header.html");
+        $("#footer").load("module/footer.html");
+    });
