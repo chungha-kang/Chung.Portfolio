2c915e2d2b85603b69c2ffaf75e2c1bd6ccb5af1
chungha-kang <cubone89@gmail.com>
2023-02-22 18:39:45 +0900

    초기 레이아웃 및 컴포넌트 생성

--- 3.todo_list/src/App.js
+++ 3.todo_list/src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+
+function Header(props) {
+  console.log('props', props)
+  return <header>
+    <p>Date</p>
+    <h2>TODO LIST</h2>
+  </header>
+}
+function Input() {
+  return <>
+    <input type="text" value="할 일을 입력하세요" />
+    <input type="button" value="+" />
+  </>
+}
+function List() {
+  return <ul>
+    <li><input type="checkbox" /><span>할일1</span><input type="checkbox" value="⭐️"/><input type="button" value="-"></input></li>
+    <li><input type="checkbox" /><span>할일2</span><input type="checkbox" value="⭐️"/><input type="button" value="-"></input></li>
+    <li><input type="checkbox" /><span>할일3</span><input type="checkbox" value="⭐️"/><input type="button" value="-"></input></li>
+  </ul>
+}
+function Create() {
+  return <form onSubmit={event=>{
+    const todo = event.target.todo.value;
+
+  }}>
+
+  </form>
+}
 
 function App() {
+  const [mode, setMode] = useState('CREATE');
+  const [id, setId] = useState(null);
+  const [nextId, setNextId] = useState(4);
+  const [topics, setTopics] = useState([
+    {id:1, todo: '할일1', import: 'Y'},
+    {id:2, todo: '할일2', import: 'N'},
+    {id:3, todo: '할일3', import: 'N'},
+  ]);
+  
+  let content = null;
+
+  // TodoList(할일)추가 (CREATE)
+  if (mode === 'CREATE') {
+    content = <Create onCreate={(_todo, _import)=>{
+
+    }}></Create>
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="list-wrap">
+      <div className="list">
+        <Header value="d"></Header>
+        <Input></Input>
+        <List></List>
+      </div>
     </div>
   );
 }
