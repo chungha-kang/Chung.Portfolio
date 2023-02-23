7b7ee64602b20d776b7108ab278bda1b755b9dc2
chungha-kang <cubone89@gmail.com>
2023-02-23 18:36:45 +0900

    todoList(할일) 누르면 detail READ하는 기능 구현중1

--- 3.todo_list/src/App.js
+++ 3.todo_list/src/App.js
@@ -2,11 +2,20 @@ import './App.css';
 import {useState} from 'react';
 
 function Header(props) {
-  console.log('props', props)
   return <header>
-    <p>Date</p>
-    <h2>TODO LIST</h2>
+    <p>{Date}(Date)</p>
+    <h2><a href='/'>{props.title}</a></h2>
   </header>
+}
+
+// TODO LIST CREATE(할일 추가)
+function Create(props) {
+  return <form onSubmit={event=>{
+    console.log(props);
+    const todo = event.target.todo.value;
+
+  }}>
+  </form>
 }
 function Input() {
   return <>
@@ -14,46 +23,78 @@ function Input() {
     <input type="button" value="+" />
   </>
 }
-function List() {
+
+// TODO LIST(할일 리스트)
+function List(props) {
+  const lis = []
+  for(let i=0; i<props.topics.length; i++) {
+    let t = props.topics[i];
+    lis.push(<>
+      <li key={t.id}>
+        <input type="checkbox" />
+        <a id={t.id} href="/" onClick={(event)=> {
+          event.preventDefault();
+          props.onChangeMode(Number(event.target.id));
+        }}>{t.todo}</a>
+        <input type="checkbox" value="⭐️" />
+        <input type="button" value="-" />
+      </li>
+      <li key={t.detail}>
+        {props.content}
+      </li>
+    </>)
+  }
   return <ul>
-    <li><input type="checkbox" /><span>할일1</span><input type="checkbox" value="⭐️"/><input type="button" value="-"></input></li>
-    <li><input type="checkbox" /><span>할일2</span><input type="checkbox" value="⭐️"/><input type="button" value="-"></input></li>
-    <li><input type="checkbox" /><span>할일3</span><input type="checkbox" value="⭐️"/><input type="button" value="-"></input></li>
+    {lis}
   </ul>
 }
-function Create() {
-  return <form onSubmit={event=>{
-    const todo = event.target.todo.value;
-
-  }}>
-
-  </form>
+function Todo(props) {
+  return <article>
+    <h2>{props.todo}</h2>
+    {props.detail}
+  </article>
 }
 
 function App() {
-  const [mode, setMode] = useState('CREATE');
+  const [mode, setMode] = useState('NON');
   const [id, setId] = useState(null);
-  const [nextId, setNextId] = useState(4);
+  // const [nextId, setNextId] = useState(4);
   const [topics, setTopics] = useState([
-    {id:1, todo: '할일1', import: 'Y'},
-    {id:2, todo: '할일2', import: 'N'},
-    {id:3, todo: '할일3', import: 'N'},
+    {id:1, todo: '할일1', detail: '자세한 내용1', check: 'true', import: 'true'},
+    {id:2, todo: '할일2', detail: '자세한 내용2', check: 'true', import: 'false'},
+    {id:3, todo: '할일3', detail: '자세한 내용3', check: 'false', import: 'false'},
   ]);
-  
+  // TodoList READ (할일 자세히 보기)
   let content = null;
-
-  // TodoList(할일)추가 (CREATE)
-  if (mode === 'CREATE') {
-    content = <Create onCreate={(_todo, _import)=>{
-
-    }}></Create>
+  if (mode === 'NON') {
+  // } else if(mode === 'DETAIL') {
+  } else if(mode === 'DETAIL' && id !== null) {
+    let todo, detail = null;
+    for(let i=0; i<topics.length; i++) {
+      if(topics[i].id === id) {
+        todo = topics[i].todo;
+        detail = topics[i].detail;
+      }
+    }
+    content = <Todo todo={todo} detail={detail}></Todo>
   }
+  // TodoList CREATE (할일 추가)
   return (
     <div className="list-wrap">
       <div className="list">
-        <Header value="d"></Header>
+        <Header title="TODO LIST"></Header>
         <Input></Input>
-        <List></List>
+        {/* text-decoration-line: line-through 만들기 */}
+        <List topics={topics} content={content} onChangeMode={(clickedId)=> {
+          if (mode === 'NON' || id != clickedId) {
+            setMode('DETAIL');
+            setId(clickedId);
+          } else {
+            setMode('NON');
+            setId(null);
+          }
+        }}></List>
+        {content}
       </div>
     </div>
   );
